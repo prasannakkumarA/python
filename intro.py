38f5aa4d29d156a8bfd897849c2e728aa056b1c2
Your Name <you@example.com>
2026-02-05 16:56:43 +0530

    intro and calculator

---
+++ intro.py
@@ -0,0 +1,19 @@
+# printing codes and finding types
+
+std_name="pk"
+std_age=22
+std_degree="b.sc"
+std_percent=80.4
+std_information=True
+
+print("NAME:",std_name)
+print("AGE:",std_age)
+print("DEG:",std_degree)
+print("PER:",std_percent)
+print("INFO:",std_information)
+
+print (type(std_name))
+print (type(std_age))
+print (type(std_degree))
+print (type(std_percent))
+print (type(std_information))
