b37cb0330a4d56c1bc49f2c9a8e8e4118fea7df0
Your Name <you@example.com>
2026-02-06 19:35:09 +0530

    area of folmulas

---
+++ area fomulas.py
@@ -0,0 +1,42 @@
+#area of rectangle l*b
+len=float(input("ent len:"))
+bri=float(input("ent bit:"))
+area=len*bri
+print("area of rectangle:",area)
+
+#area of square a*a
+squ=float(input("square area:"))
+formula=squ*squ
+print("area of square:",formula)
+
+#area of triangle 0.5*b*h
+bre=float(input("triangle breath:"))
+hei=float(input("height:"))
+formula=0.5*bre*hei
+print("area of triangle:",formula)
+
+#area of circle pi*r*r
+rad=float(input("circule radius:"))
+formula=22/7*rad*rad
+print("area of cicule:",formula)
+
+#area of cone 1/3*pi*r*r*h
+rad=float(input("cone radius:"))
+hei=float(input("cone height:"))
+formula=1/3*3.14*rad*rad*hei
+print("area of cone:",formula)
+
+#perimeter of circle 2*pi*r
+radius=float(input("peri of circule radius:"))
+formula=2*22/7*radius
+print("perimeter of circle:",formula)
+
+#perimeter of square 4*a
+area=float(input("pri of square area:"))
+formula=4*area
+print("perimeter of square:",formula)
+
+
+
+
+
