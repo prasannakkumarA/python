8b7f53c9c127704a60df8681e1f35f077f2e0b54
Your Name <you@example.com>
2026-02-07 16:03:24 +0530

    palindrome and measures

---
+++ measures ex2.py
@@ -0,0 +1,41 @@
+#feet to cm
+ft=30.48
+cm=int(input("enter feet:"))
+ans=cm*ft
+print("total cm's:",ans)
+
+#meter to cm
+mt=100
+cm=int(input("enter mtr:"))
+ans=cm*mt
+print("toatl cm's",ans)
+
+#inch to cm
+inch=2.54
+cm=int(input("enter inch's:"))
+ans=cm*inch
+print("total cm's:",ans)
+
+#km to meter
+km=1000
+mtr=int(input("enter km:"))
+ans=mtr*km
+print("toatl meter's:",ans)
+
+#milli mtr to cm
+mm=10
+cm=int(input("enter mm:"))
+ans=cm//mm
+print("toatl cm's:",ans)
+
+#feet to inch
+ft=12
+inch=int(input("enter ft:"))
+ans=inch*ft
+print("toatl inch's:",ans)
+
+
+
+
+
+
---
+++ palindrome ex1.py
@@ -0,0 +1,6 @@
+word=input("enter word:")
+if word==word[::-1]:
+    print("this word is palingrome")
+else:
+    print("this word is  not a palingrome")
+    
